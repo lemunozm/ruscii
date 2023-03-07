f99020063e13efeabb676d770c80ba8b8425a88a
Gavin Tran <gavinpha@outlook.com>
2023-03-07 03:50:16 -0500

    Added documentation for several files. (#10)
    * Added documentation for spatial.rs, lib.rs, and gui.rs.
    
    * Added slight correction to Vec2 documentation.
    
    * Added documentation for drawing.rs.
    
    * Applied linter to spatial.rs and pong.rs.
    
    * Made slight correction to RectCharset::from documentation.
    
    * Update examples/pong.rs
    
    Co-authored-by: Luis Enrique Muñoz Martín <lemunozm@gmail.com>
    
    ---------
    
    Co-authored-by: Luis Enrique Muñoz Martín <lemunozm@gmail.com>

--- src/gui.rs
+++ src/gui.rs
@@ -1,5 +1,45 @@
+//! # GUI
+//!
+//! The `gui` module provides utilities for common GUI elements. Currently it includes an
+//! [FPSCounter] that provides the user easy access to the application framerate without them
+//! needing to make any time calculations.
+//!
+//! More examples of use of the [FPSCounter] can be found in the
+//! [examples](https://github.com/lemunozm/ruscii/tree/master/examples) folder of the
+//! [ruscii](https://github.com/lemunozm/ruscii) repository.
+
 use std::time::{SystemTime};
 
+/// A struct that provides access to the application's framerate.
+///
+/// This FPS counter does not update automatically. [update()](FPSCounter::update) should be called
+/// to update the FPS for every frame, i.e., in the `frame_action` function parameter of
+/// [App::run](super::app::App::run).
+///
+/// The FPS value can be obtained by calling [count()](FPSCounter::count).
+///
+/// # Example
+///
+/// ```rust,no_run
+/// # use ruscii::app::{App, State};
+/// # use ruscii::terminal::Window;
+/// # use ruscii::drawing::{Pencil};
+/// # use ruscii::spatial::{Vec2};
+/// # use ruscii::gui::{FPSCounter};
+///
+/// let mut app = App::new();
+/// let mut fps_counter = FPSCounter::new();
+///
+/// app.run(|app_state: &mut State, window: &mut Window| {
+///     fps_counter.update();  // Updates the FPS
+///
+///     let mut pencil = Pencil::new(window.canvas_mut());
+///     pencil.draw_text(
+///         &format!("FPS: {}", fps_counter.count()),  // Draws the FPS
+///         Vec2::xy(1, 0)
+///     );
+/// });
+/// ```
 pub struct FPSCounter {
     fps: u32,
     fps_time_stamp: u128,
@@ -7,14 +47,16 @@ pub struct FPSCounter {
 }
 
 impl FPSCounter {
+    /// Creates a new [FPSCounter] with default values.
     pub fn new() -> FPSCounter {
         FPSCounter {
             fps: 0,
             fps_time_stamp: 0,
-            frame_counter: 0
+            frame_counter: 0,
         }
     }
 
+    /// Retrieves the framerate and updates the [FPSCounter].
     pub fn update(&mut self) {
         let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_micros();
         self.frame_counter += 1;
@@ -25,8 +67,8 @@ impl FPSCounter {
         }
     }
 
+    /// Returns the framerate.
     pub fn count(&self) -> u32 {
         self.fps
     }
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! # `ruscii`
+//!
+//! `ruscii` is a library that aims to provide a fast, straightforward, and easy-to-use interface
+//! with the terminal to create text-based games and applications. It can be used on multiple
+//! platforms and terminals. For more information, see
+//! [Crossterm's terminal support](https://github.com/crossterm-rs/crossterm#tested-terminals).
+//!
+//! `ruscii`'s simple API for key press/release events and drawing on the terminal screen enables
+//! effortless and quick game development in relatively few lines. For examples, see the
+//! [examples](https://github.com/lemunozm/ruscii/tree/master/examples) folder of the
+//! [ruscii](https://github.com/lemunozm/ruscii) repository.
+
 pub mod app;
 pub mod gui;
 pub mod terminal;
